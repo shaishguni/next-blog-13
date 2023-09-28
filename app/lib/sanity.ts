0d0a0f00682967d962cb0f59e1137dd480e0ff35
Shaish Guni <gunisguni2007@gmail.com>
2023-09-28 21:05:14 +0545

    first commit

---
+++ app/lib/sanity.ts
@@ -0,0 +1,12 @@
+import { createClient } from "next-sanity"
+
+const projectId="2fx9bz0i"
+const dataset = "production"
+const apiVersion = "2023-01-01"
+
+export const client = createClient(
+    projectId,
+    dataset,
+    apiVersion,
+    useCDN:true,
+)
---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '2fx9bz0i',
+    dataset: 'production'
+  }
+})
